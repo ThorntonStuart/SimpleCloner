3289a05f00c8e645e61608744c4f61c4db6c76ca
Stuart Thornton <thorntonstuart@googlemail.com>
2016-12-12 15:22:54 +0000

    Updated version number in config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ if (! defined('SIMPLE_CLONER_KEY'))
 {
 	define('SIMPLE_CLONER_NAME', 'Simple Cloner');
 	define('SIMPLE_CLONER_NAME_FORMATTED', 'Simple_cloner');
-	define('SIMPLE_CLONER_VER',  '1.0.0');
+	define('SIMPLE_CLONER_VER',  '1.1.0');
 	define('SIMPLE_CLONER_KEY', 'simple_cloner');
 	define('SIMPLE_CLONER_AUTHOR',  'Stuart Thornton');
 	define('SIMPLE_CLONER_DOCS',  'http://www.github.com/ThorntonStuart');
